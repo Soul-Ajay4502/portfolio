6d15663212f50daad074692f21bd212d671a1008
ajay <ajay@beurokrat.com>
2025-06-17 10:41:36 +0530

    feat: remove next-sitemap dependency and related files; update sitemap structure for improved SEO

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://portfolio-ajay-psi.vercel.app/', // 🔁 Replace with your real domain
-    generateRobotsTxt: true,            // ✅ Generate robots.txt file
-    sitemapSize: 5000,                  // Optional: splits into multiple files if needed
-    changefreq: 'monthly',
-    priority: 0.7,
-}
